b552d08680d9f0c2c49684b1c13cb61705ca6007
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-03 20:57:09 -0300

    feater: products styles

--- src/pages/homePage/components/Products.tsx
+++ src/pages/homePage/components/Products.tsx
@@ -8,66 +8,82 @@ export const Products = () => {
 
       <ul className={styles.productsList}>
         <article className={styles.product}>
+            <span className={styles.detail}>-30%</span>
+
             <figure className={styles.productImg}>
                 <img src={img} alt="product" />
             </figure>
 
-            <h2 className={styles.name}>Syltherine</h2>
+            <div className={styles.info}>
+              <h2 className={styles.name}>Syltherine</h2>
 
-            <h3 className={styles.legend}>Stylish cafe chair</h3>
+              <h3 className={styles.legend}>Stylish cafe chair</h3>
 
-            <div className={styles.priceContainer}>
-              <span className={styles.currentPrice}>Rp 2.500.000</span>
+              <div className={styles.priceContainer}>
+                <span className={styles.currentPrice}>Rp 2.500.000</span>
 
-              <span className={styles.oldPrice}>Rp 3.500.000</span>
+                <span className={styles.oldPrice}>Rp 3.500.000</span>
+              </div>
             </div>
         </article>
         
         <article className={styles.product}>
+            <span className={styles.detail}>-30%</span>
+
             <figure className={styles.productImg}>
                 <img src={img} alt="product" />
             </figure>
 
-            <h2 className={styles.name}>Syltherine</h2>
+            <div className={styles.info}>
+              <h2 className={styles.name}>Syltherine</h2>
 
-            <h3 className={styles.legend}>Stylish cafe chair</h3>
+              <h3 className={styles.legend}>Stylish cafe chair</h3>
 
-            <div className={styles.priceContainer}>
-              <span className={styles.currentPrice}>Rp 2.500.000</span>
+              <div className={styles.priceContainer}>
+                <span className={styles.currentPrice}>Rp 2.500.000</span>
 
-              <span className={styles.oldPrice}>Rp 3.500.000</span>
+                <span className={styles.oldPrice}>Rp 3.500.000</span>
+              </div>
             </div>
         </article>
         
         <article className={styles.product}>
+            <span className={styles.detail}>-30%</span>
+
             <figure className={styles.productImg}>
                 <img src={img} alt="product" />
             </figure>
 
-            <h2 className={styles.name}>Syltherine</h2>
+            <div className={styles.info}>
+              <h2 className={styles.name}>Syltherine</h2>
 
-            <h3 className={styles.legend}>Stylish cafe chair</h3>
+              <h3 className={styles.legend}>Stylish cafe chair</h3>
 
-            <div className={styles.priceContainer}>
-              <span className={styles.currentPrice}>Rp 2.500.000</span>
+              <div className={styles.priceContainer}>
+                <span className={styles.currentPrice}>Rp 2.500.000</span>
 
-              <span className={styles.oldPrice}>Rp 3.500.000</span>
+                <span className={styles.oldPrice}>Rp 3.500.000</span>
+              </div>
             </div>
         </article>
         
         <article className={styles.product}>
+            <span className={styles.detail}>-30%</span>
+
             <figure className={styles.productImg}>
                 <img src={img} alt="product" />
             </figure>
 
-            <h2 className={styles.name}>Syltherine</h2>
+            <div className={styles.info}>
+              <h2 className={styles.name}>Syltherine</h2>
 
-            <h3 className={styles.legend}>Stylish cafe chair</h3>
+              <h3 className={styles.legend}>Stylish cafe chair</h3>
 
-            <div className={styles.priceContainer}>
-              <span className={styles.currentPrice}>Rp 2.500.000</span>
+              <div className={styles.priceContainer}>
+                <span className={styles.currentPrice}>Rp 2.500.000</span>
 
-              <span className={styles.oldPrice}>Rp 3.500.000</span>
+                <span className={styles.oldPrice}>Rp 3.500.000</span>
+              </div>
             </div>
         </article>
       </ul>
