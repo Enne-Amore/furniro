6d79fc7bb5a174732a47f354159334948fd38adc
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-03 15:58:30 -0300

    feater: structuring products

--- src/pages/homePage/components/Main.tsx
+++ src/pages/homePage/components/Main.tsx
@@ -1,10 +1,16 @@
 import { Intro } from "./Intro";
 import styles from "./Main.module.css";
+import { Products } from "./Products";
+import Range from "./Range";
 
 export const Main = () => {
   return (
     <main className={styles.container}>
       <Intro />
+
+      <Range />
+
+      <Products />
     </main>
   );
 };
---
+++ src/pages/homePage/components/Products.tsx
@@ -0,0 +1,76 @@
+import styles from './Products.module.css';
+import img from '../../../assets/syltherine-item.png'
+
+export const Products = () => {
+  return (
+    <section className={styles.productsContainer}>
+      <h1 className={styles.title}>Our Products</h1>
+
+      <ul className={styles.productsList}>
+        <article className={styles.product}>
+            <figure className={styles.productImg}>
+                <img src={img} alt="product" />
+            </figure>
+
+            <h2 className={styles.name}>Syltherine</h2>
+
+            <h3 className={styles.legend}>Stylish cafe chair</h3>
+
+            <div className={styles.priceContainer}>
+              <span className={styles.currentPrice}>Rp 2.500.000</span>
+
+              <span className={styles.oldPrice}>Rp 3.500.000</span>
+            </div>
+        </article>
+        
+        <article className={styles.product}>
+            <figure className={styles.productImg}>
+                <img src={img} alt="product" />
+            </figure>
+
+            <h2 className={styles.name}>Syltherine</h2>
+
+            <h3 className={styles.legend}>Stylish cafe chair</h3>
+
+            <div className={styles.priceContainer}>
+              <span className={styles.currentPrice}>Rp 2.500.000</span>
+
+              <span className={styles.oldPrice}>Rp 3.500.000</span>
+            </div>
+        </article>
+        
+        <article className={styles.product}>
+            <figure className={styles.productImg}>
+                <img src={img} alt="product" />
+            </figure>
+
+            <h2 className={styles.name}>Syltherine</h2>
+
+            <h3 className={styles.legend}>Stylish cafe chair</h3>
+
+            <div className={styles.priceContainer}>
+              <span className={styles.currentPrice}>Rp 2.500.000</span>
+
+              <span className={styles.oldPrice}>Rp 3.500.000</span>
+            </div>
+        </article>
+        
+        <article className={styles.product}>
+            <figure className={styles.productImg}>
+                <img src={img} alt="product" />
+            </figure>
+
+            <h2 className={styles.name}>Syltherine</h2>
+
+            <h3 className={styles.legend}>Stylish cafe chair</h3>
+
+            <div className={styles.priceContainer}>
+              <span className={styles.currentPrice}>Rp 2.500.000</span>
+
+              <span className={styles.oldPrice}>Rp 3.500.000</span>
+            </div>
+        </article>
+      </ul>
+    </section>
+  )
+}
