dd26c852a12136e22e14a0bcb837eaf872ac2700
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-05 14:42:48 -0300

    fix: name file of main section

--- src/pages/checkoutPage/Main.tsx
+++ src/pages/checkoutPage/Main.tsx
@@ -1,5 +1,6 @@
 import { Advantages } from '../../components/advantages/Advantages'
 import { WherePage } from '../../components/wherePage/WherePage'
+import { GeneralForm } from './components/GeneralForm'
 import styles from './Main.module.css'
 
 export const Main = () => {
@@ -7,7 +8,7 @@ export const Main = () => {
     <main className={styles.container}>
       <WherePage namePage={'Cart'} />
 
-      
+      <GeneralForm />      
 
       <Advantages />
     </main>
--- src/pages/checkoutPage/components/Form.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Form.module.css'
-
-export const Form = () => {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
-}
