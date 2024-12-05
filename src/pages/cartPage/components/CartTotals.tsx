a6de567fa7c7cbbe6d24d284e60a58ae52589fcf
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-05 07:01:31 -0300

    feater: structuring CartTotals component

--- src/pages/cartPage/components/CartTotals.tsx
+++ src/pages/cartPage/components/CartTotals.tsx
@@ -3,7 +3,21 @@ import styles from './CartTotals.module.css'
 export const CartTotals = () => {
   return (
     <div className={styles.container}>
-      
+      <h1 className={styles.title}>Cart Totals</h1>
+
+      <div className={styles.infoContainer}>
+        <strong className={styles.strong}>Subtotal</strong>
+        
+        <span className={styles.data}>Rs. 250,000.00</span>
+      </div>
+
+      <div className={styles.infoContainer}>
+        <strong className={styles.strong}>Total</strong>
+        
+        <span className={styles.data}>Rs. 250,000.00</span>
+      </div>
+
+      <button type="button" className={styles.btn}>Check Out</button>
     </div>
   )
 }
