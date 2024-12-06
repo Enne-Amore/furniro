9097cd3b1c6c9904a38a03a5643444ca161b2897
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-06 13:32:41 -0300

    feater: styles login page

--- src/pages/loginPage/components/Form.tsx
+++ src/pages/loginPage/components/Form.tsx
@@ -2,34 +2,38 @@ import styles from "./Form.module.css";
 
 export const Form = () => {
   return (
-    <form>
-      <div className={styles.inputContainer}>
-        <label htmlFor="email" className={styles.label}>
-          Email
-        </label>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Login Information</h1>
 
-        <input
-          type="email"
-          id="email"
-          placeholder="Enter your email address"
-          className={styles.input}
-        />
-      </div>
+      <form className={styles.formContainer}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="email" className={styles.label}>
+            Email
+          </label>
 
-      <div className={styles.inputContainer}>
-        <label htmlFor="password" className={styles.label}>
-          Password
-        </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your email address"
+            className={styles.input}
+          />
+        </div>
 
-        <input
-          type="password"
-          id="password"
-          placeholder="Enter your password"
-          className={styles.input}
-        />
-      </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="password" className={styles.label}>
+            Password
+          </label>
 
-      <button type="submit" className={styles.btn}>Login</button>
-    </form>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            className={styles.input}
+          />
+        </div>
+
+        <button type="submit" className={styles.btn}>Submit</button>
+      </form>
+    </section>
   );
 };
