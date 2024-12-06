17873d48baa0fc3fa3fb97d031071222aff04606
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-06 17:44:04 -0300

    feater: styles contact page

--- src/pages/contactPage/components/Info.tsx
+++ src/pages/contactPage/components/Info.tsx
@@ -9,7 +9,7 @@ export const Info = () => {
       <article className={styles.info}>
         <img src={localIcon} alt="Local icon" />
 
-        <div className={styles.texts}>
+        <div className={`${styles.texts} ${styles.address}`}>
           <h2 className={styles.title}>Address</h2>
           
           <span className={styles.data}>236 5th SE Avenue, New York NY10000, United States</span>
@@ -19,7 +19,7 @@ export const Info = () => {
       <article className={styles.info}>
         <img src={phoneIcon} alt="Phone icon" />
 
-        <div className={styles.texts}>
+        <div className={`${styles.texts} ${styles.phone}`}>
           <h2 className={styles.title}>Phone</h2>
           
           <span className={styles.data}>Mobile: +(84) 546-6789</span>
@@ -31,7 +31,7 @@ export const Info = () => {
       <article className={styles.info}>
         <img src={clockIcon} alt="Clock icon" />
 
-        <div className={styles.texts}>
+        <div className={`${styles.texts} ${styles.time}`}>
           <h2 className={styles.title}>Working Time</h2>
           
           <span className={styles.data}>Monday-Friday: 9:00 - 22:00</span>
