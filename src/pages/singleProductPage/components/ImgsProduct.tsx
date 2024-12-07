4ff227b4062a48f10007ded24289aad0bce2e74c
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-07 15:33:46 -0300

    feater: styles ImgProduct component

--- src/pages/singleProductPage/components/ImgsProduct.tsx
+++ src/pages/singleProductPage/components/ImgsProduct.tsx
@@ -1,28 +1,29 @@
+import exampleImg from '../../../assets/example-img.png';
 import styles from './ImgsProduct.module.css'
 
 export const ImgsProduct = () => {
   return (
     <article className={styles.container}>
-      <ul>
-        <figure>
-          <img src="" alt="" />
+      <ul className={styles.imgList}>
+        <figure className={styles.thumbnailImg}>
+          <img src={exampleImg} alt="" />
         </figure>
 
-        <figure>
-          <img src="" alt="" />
+        <figure className={styles.thumbnailImg}>
+          <img src={exampleImg} alt="" />
         </figure>
 
-        <figure>
-          <img src="" alt="" />
+        <figure className={styles.thumbnailImg}>
+          <img src={exampleImg} alt="" />
         </figure>
 
-        <figure>
-          <img src="" alt="" />
+        <figure className={styles.thumbnailImg}>
+          <img src={exampleImg} alt="" />
         </figure>
       </ul>
 
-      <figure>
-        <img src="" alt="" />
+      <figure className={styles.imgSelected}>
+        <img src={exampleImg} alt="" />
       </figure>
     </article>
   )
