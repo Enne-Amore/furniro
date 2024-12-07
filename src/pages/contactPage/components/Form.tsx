8bb25bdcfdb85943af694734974285848a070dfc
Enne Jim Pessoa <ennefofe10@gmail.com>
2024-12-07 09:32:13 -0300

    feater: validation contact form

--- src/pages/contactPage/components/Form.tsx
+++ src/pages/contactPage/components/Form.tsx
@@ -1,18 +1,36 @@
+import { useFormsContact } from '../../../hooks/UseFormsContact';
+import { FormContact } from '../../../schema/FormContact';
 import styles from './Form.module.css'
 
 export const Form = () => {
+  const { register, handleSubmit, errors } = useFormsContact();
+
+  const onSubmit = (data: FormContact) => {};
+  
   return (
-    <form className={styles.container}>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
       <div className={styles.inputContainer}>
         <label htmlFor="name" className={styles.label}>Your name</label>
 
-        <input type="text" id='name' placeholder='Abc' className={styles.input} /> 
+        <div className={styles.withErrorsContainer}>
+          <input type="text" id='name' {...register("name")} placeholder='Abc' className={styles.input} />
+
+          {errors.name && (
+              <span className={styles.errors}>{errors.name?.message}</span>
+          )}
+        </div>
       </div>
 
       <div className={styles.inputContainer}>
         <label htmlFor="email" className={styles.label}>Email address</label>
 
-        <input type="email" id='email' placeholder='Abc@def.com' className={styles.input} />
+        <div className={styles.withErrorsContainer}>
+          <input type="email" id='email' {...register("email")} placeholder='Abc@def.com' className={styles.input} />
+
+          {errors.email && (
+              <span className={styles.errors}>{errors.email?.message}</span>
+          )}
+        </div>
       </div>
 
       <div className={styles.inputContainer}>
@@ -24,7 +42,13 @@ export const Form = () => {
       <div className={styles.inputContainer}>
         <label htmlFor="message" className={styles.label}>Message</label>
 
-        <input type="text" id='message' placeholder='Hi! i’d like to ask about' className={styles.input} />
+        <div className={styles.withErrorsContainer}>
+          <textarea id='message' {...register("message")} maxLength={300} placeholder='Hi! i’d like to ask about' className={`${styles.input} h-28 resize-none`} />
+
+          {errors.message && (
+              <span className={styles.errors}>{errors.message?.message}</span>
+          )}
+        </div>
       </div>
 
       <button type="submit" className={styles.btn}>Submit</button>
